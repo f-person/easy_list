3322547a27f3db1d564fe65e6f438f59ec96dca3
f-person <agakaryan.arshak@gmail.com>
2019-05-25 19:59:14 +0400

    Improve AuthPage

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 
-// import './products.dart';
-
 class AuthPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('login'),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/');
-          },
+        appBar: AppBar(
+          title: Text('Login'),
         ),
-      ),
-    );
+        body: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            RaisedButton(
+              color: Theme.of(context).accentColor,
+              textColor: Colors.white,
+              child: Text('Login'),
+              onPressed: () => Navigator.pushReplacementNamed(context, '/'),
+            )
+          ],
+        ));
   }
 }
