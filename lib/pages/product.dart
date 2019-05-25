156e366fac45b41881c38c1e9df6b12c6a494f79
f-person <agakaryan.arshak@gmail.com>
2019-05-25 07:18:11 +0400

    Add login page and 'delete' feature

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,20 +1,43 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  ProductPage(this.title, this.imageUrl);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ProductDetail'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Text("Details!"),
-          RaisedButton(
-            child: Text('քամակ'),
-            onPressed: () => Navigator.pop(context),
-          )
-        ],
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(imageUrl),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(title),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme.of(context).accentColor,
+                textColor: Colors.white,
+                child: Text('delete'),
+                onPressed: () => Navigator.pop(context, true),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/product_control.dart
+++ lib/product_control.dart
@@ -6,13 +6,18 @@ class ProductControl extends StatelessWidget {
   ProductControl(this.addProduct);
 
   @override
-    Widget build(BuildContext context) {
-      return RaisedButton(
-            color: Theme.of(context).primaryColor,
-            onPressed: () {
-              addProduct('Sweets');
-            },
-            child: Text('Add Product'),
-          );
-    }
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      color: Theme.of(context).primaryColor,
+      onPressed: () {
+        addProduct(
+          {
+            'title': 'Chocolate',
+            'image': 'assets/food.jpg',
+          },
+        );
+      },
+      child: Text('Add Product'),
+    );
+  }
 }
