eb383402afa3308d4b42d297bd44ec3b5c456676
f-person <agakaryan.arshak@gmail.com>
2019-05-25 20:02:07 +0400

    Extend ProductCreatePage

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,20 +1,74 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  final Function addProduct;
+
+  ProductCreatePage(this.addProduct);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePage();
+  }
+}
+
+class _ProductCreatePage extends State<ProductCreatePage> {
+  String _titleValue;
+  String _descriptionValue;
+  double _priceValue;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Center(
-                  child: Text("This is a Modal"),
-                );
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Product Title',
+            ),
+            autofocus: true,
+            onChanged: (String value) {
+              setState(() {
+                _titleValue = value;
               });
-        },
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Product Desription',
+            ),
+            keyboardType: TextInputType.multiline,
+            maxLines: 4,
+            onChanged: (String value) {
+              _descriptionValue = value;
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Product Price',
+            ),
+            keyboardType: TextInputType.number,
+            onChanged: (String value) {
+              _priceValue = double.parse(value);
+            },
+          ),
+          SizedBox(height: 10.0),
+          RaisedButton(
+            color: Theme.of(context).accentColor,
+            textColor: Colors.white,
+            child: Text('Save'),
+            onPressed: () {
+              final Map<String, dynamic> product = {
+                'title': _titleValue,
+                'description': _descriptionValue,
+                'price': _priceValue,
+                'image': 'assets/food.jpg'
+              };
+              widget.addProduct(product);
+              Navigator.pushReplacementNamed(context, '/');
+            },
+          )
+        ],
       ),
     );
   }
