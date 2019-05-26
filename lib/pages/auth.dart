99239c4600ca30aa0f1c28f3e1eedab6a6dd298e
f-person <agakaryan.arshak@gmail.com>
2019-05-26 15:14:52 +0400

    Store email and password values

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
+}
+
+class _AuthPageState extends State<AuthPage> {
+  String _emailValue = '';
+  String _passwordValue = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,25 +19,34 @@ class AuthPage extends StatelessWidget {
       ),
       body: Container(
         padding: EdgeInsets.all(15.0),
-        child: Column(
+        child: ListView(
           children: <Widget>[
             TextField(
-              decoration: InputDecoration(labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-            ),
+                decoration: InputDecoration(labelText: 'E-Mail'),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (String value) {
+                  setState(() {
+                    _emailValue = value;
+                  });
+                }),
             TextField(
               decoration: InputDecoration(labelText: 'Password'),
               obscureText: true,
+              onChanged: (String value) {
+                setState(() {
+                  _passwordValue = value;
+                });
+              },
             ),
             Container(
               padding: EdgeInsets.only(top: 5.0),
               child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                textColor: Colors.white,
-                child: Text('Login'),
-                onPressed: () =>
-                    Navigator.pushReplacementNamed(context, '/products'),
-              ),
+                  color: Theme.of(context).accentColor,
+                  textColor: Colors.white,
+                  child: Text('Login'),
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/products');
+                  }),
             )
           ],
         ),
