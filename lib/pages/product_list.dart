71ed78af83226f015b4dd1ac26cdd5737ed7178e
f-person <agakaryan.arshak@gmail.com>
2019-06-06 14:07:40 +0400

    Deal with State Management with Scoped Model

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,59 +1,61 @@
 import 'package:flutter/material.dart';
 
+import 'package:scoped_model/scoped_model.dart';
+
 import 'package:EasyList/pages/product_edit.dart';
-import '../models/product.dart';
+import '../scoped-models/products.dart';
 
 class ProductListPage extends StatelessWidget {
-  final List<Product> products;
-  final Function updateProduct;
-  final Function deleteProduct;
-
-  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
-
-  Widget _buildEditButton(BuildContext context, int index) {
+  Widget _buildEditButton(
+      BuildContext context, int index, ProductsModel model) {
     return IconButton(
-      icon: Icon(Icons.edit),
-      onPressed: () {
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (BuildContext context) {
-            return ProductEditPage(
-              product: products[index],
-              updateProduct: updateProduct,
-              productIndex: index,
-            );
-          },
-        ));
-      },
-    );
+        icon: Icon(Icons.edit),
+        onPressed: () {
+          model.selectProduct(index);
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext context) {
+                return ProductEditPage();
+              },
+            ),
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (BuildContext context, int index) {
-        return Dismissible(
-          background: Container(color: Colors.red),
-          onDismissed: (DismissDirection direction) {
-            if (direction == DismissDirection.endToStart) {
-              deleteProduct(index);
-            }
-          },
-          key: Key(products[index].title),
-          child: Column(
-            children: <Widget>[
-              ListTile(
-                leading: CircleAvatar(
-                    backgroundImage: AssetImage(products[index].image)),
-                title: Text(products[index].title),
-                subtitle: Text('\$${products[index].price.toString()}'),
-                trailing: _buildEditButton(context, index),
+    return ScopedModelDescendant<ProductsModel>(
+      builder: (BuildContext context, Widget child, ProductsModel model) {
+        return ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+              background: Container(color: Colors.red),
+              onDismissed: (DismissDirection direction) {
+                model.selectProduct(index);
+                if (direction == DismissDirection.endToStart) {
+                  model.deleteProduct();
+                }
+              },
+              key: Key(model.products[index].title),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                        backgroundImage:
+                            AssetImage(model.products[index].image)),
+                    title: Text(model.products[index].title),
+                    subtitle:
+                        Text('\$${model.products[index].price.toString()}'),
+                    trailing: _buildEditButton(context, index, model),
+                  ),
+                  Divider(),
+                ],
               ),
-              Divider(),
-            ],
-          ),
+            );
+          },
+          itemCount: model.products.length,
         );
       },
-      itemCount: products.length,
     );
   }
 }
---
+++ lib/scoped-models/products.dart
@@ -0,0 +1,42 @@
+import 'package:scoped_model/scoped_model.dart';
+
+import '../models/product.dart';
+
+class ProductsModel extends Model {
+  List<Product> _products = [];
+  int _selectedProductIndex;
+
+  List<Product> get products {
+    return List.from(_products);
+  }
+
+  int get selectedProductIndex {
+    return _selectedProductIndex;
+  }
+
+  Product get selectedProduct {
+    if (selectedProductIndex == null) {
+      return null;
+    }
+    return products[_selectedProductIndex];
+  }
+
+  void addProduct(Product product) {
+    _products.add(product);
+    _selectedProductIndex = null;
+  }
+
+  void updateProduct(Product product) {
+    _products[selectedProductIndex] = product;
+    _selectedProductIndex = null;
+  }
+
+  void deleteProduct() {
+    _products.removeAt(selectedProductIndex);
+    _selectedProductIndex = null;
+  }
+
+  void selectProduct(int index) {
+    _selectedProductIndex = index;
+  }
+}
